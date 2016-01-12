e307de59c3b6822e819db822c41301a92d29480e
nmalesic <nicolas.malesic@Berger-Levraul.fr>
2016-01-12 11:20:26 +0100

    Nico : Main / plusieurs cas de tests de triangle

--- src/com/nicomanu/triangle/MainClass.java
+++ src/com/nicomanu/triangle/MainClass.java
@@ -4,8 +4,20 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		Triangle triangle = new Triangle(0, 10, 10, 0, 10, 10);
-		System.out.println(triangle.identificationForme());
 		System.out.println("------------");
 		System.out.println(triangle);
+
+		Triangle triangle2 = new Triangle(0, 0, 10, 0, 10, 10);
+		System.out.println("------------");
+		System.out.println(triangle2);
+
+		Triangle triangle3 = new Triangle(0, 1, 10, 0, 10, 10);
+		System.out.println("------------");
+		System.out.println(triangle3);
+
+		Triangle triangle4 = new Triangle(0, 0, 0, 0, 0, 0);
+		System.out.println("------------");
+		System.out.println(triangle4);
+		
 	}
 }
